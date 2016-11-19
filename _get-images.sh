8b5062d4055ce0f80c2f78fec269efa7be15de36
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-11-18 19:37:14 -0500

    Added root 'manifest' file. Added first pass of 'getImages()' utility function.

---
+++ _get-images.sh
@@ -0,0 +1,27 @@
+echo ">>> LOADING 'getImages'"
+
+# ...
+function getImages {
+	hostURL="http://lorempixel.com/"
+	defaultWidth=1000
+	defaultHeight=750
+	defaultImageNum=5
+
+	if [ -z $1 ]; then
+
+		echo "DID NOT RECEIVE ANY ARGUMENTS"
+
+	else
+
+		echo "RECEIVED ARGUMENT(S)"
+		echo $1
+
+		# ... UPDATE FUNCTION TO RECEIVE ARGS.
+
+	fi
+
+	# /// TEMP
+	for i in {1..5}; do
+		curl "http://lorempixel.com/$defaultWidth/$defaultHeight/" > "image-$i.jpg"
+	done
+}

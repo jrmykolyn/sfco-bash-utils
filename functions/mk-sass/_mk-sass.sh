db51bc714f92cc6399502761744a66b35f61c116
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-11-22 21:40:20 -0500

    Updated 'my-sass' to create a new directory if called with the '-d' or '--dir' flag. @v0.2.5.

--- functions/mk-sass/_mk-sass.sh
+++ functions/mk-sass/_mk-sass.sh
@@ -14,35 +14,56 @@ function mk-sass {
   # Declare vars.
   defaultFileType='.scss'
   defaultFilePrefix='_'
+  defaultTargetDir='./'
 
   # Parse args.
   if [ -z "$1" ] || [ "$1" == "" ]; then
     echo "DID NOT RECEIVE ARGS."
   else
 
-    # Concatenate file name.
+    # Assign partial and file-related vars.
     # TODO:
-    # Update assignment below so that string concat. is more legible.
-    file="$defaultFilePrefix$1$defaultFileType"
+    # Update assignments below to be more legible.
+    partialName=$1
+    fileName="$defaultFilePrefix$partialName$defaultFileType"
 
-    # Print file to current dir.
-    touch "$file"
+    # Loop over received args.
+    for arg in $@; do
 
-    # Append file.
-    echo '// --------------------------------------------------' >> "$file"
-    echo '// VARIABLES ' >> "$file"
-    echo '// --------------------------------------------------'  >> "$file"
-    echo '// Add any SASS variables that are specific to this file here.' >> "$file"
-    printf '\n\n' >> "$file"
-    echo '// --------------------------------------------------'  >> "$file"
-    echo '// PLACEHOLDERS '  >> "$file"
-    echo '// --------------------------------------------------'  >> "$file"
-    echo '// Write shared styles in placeholder rules here, then extend them from specific selectors below.'  >> "$file"
-    printf '\n\n' >> "$file"
-    echo '// --------------------------------------------------'  >> "$file"
-    echo '// SELECTORS ' >> "$file"
-    echo '// --------------------------------------------------'  >> "$file"
-    echo '// Rules, logic, and anything else that can be compiled to "vanilla" CSS goes here.' >> "$file"
-    printf '\n' >> "$file"
+      # Parse arguments, skip argument at position 1 (eg. 'partialName').
+      if [ "$arg" != $1 ]; then
+
+        case "$arg" in
+          "-d")
+            defaultTargetDir="$partialName/"
+            [ -d "./$defaultTargetDir" ] || mkdir $defaultTargetDir
+            ;;
+          "--dir")
+            defaultTargetDir="$partialName/"
+            [ -d "./$defaultTargetDir" ] || mkdir $defaultTargetDir
+            ;;
+        esac
+      fi
+    done
+
+    # Assign 'file path' (target directory plus concat'd file name).
+    filePath="$defaultTargetDir$fileName"
+
+    # Create and append file.
+    echo '// --------------------------------------------------' >> "$filePath"
+    echo '// VARIABLES ' >> "$filePath"
+    echo '// --------------------------------------------------'  >> "$filePath"
+    echo '// Add any SASS variables that are specific to this file here.' >> "$filePath"
+    printf '\n\n' >> "$filePath"
+    echo '// --------------------------------------------------'  >> "$filePath"
+    echo '// PLACEHOLDERS '  >> "$filePath"
+    echo '// --------------------------------------------------'  >> "$filePath"
+    echo '// Write shared styles in placeholder rules here, then extend them from specific selectors below.'  >> "$filePath"
+    printf '\n\n' >> "$filePath"
+    echo '// --------------------------------------------------'  >> "$filePath"
+    echo '// SELECTORS ' >> "$filePath"
+    echo '// --------------------------------------------------'  >> "$filePath"
+    echo '// Rules, logic, and anything else that can be compiled to "vanilla" CSS goes here.' >> "$filePath"
+    printf '\n' >> "$filePath"
   fi
 }
